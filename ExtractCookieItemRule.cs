d31e6b436eb8b79520b85228c75a57c8dc78dd2a
Theodore Chatziperis <xatzipe@gmail.com>
2015-09-28 15:46:15 +0300

    created New project add Extraction Rule for extracting a JSON property from response add Extraction Rule for extracting a Cookie property from response

---
+++ ExtractCookieItemRule.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.WebTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPTCustomExtractionRules
+{
+    [DisplayName("Extract Cookie Item Rule")]
+    [Description("Extracts the specified Cookie property from an object.")]
+    public class ExtractCookieItemRule : ExtractionRule
+    {
+
+        [DisplayName("Cookie Property Name")]
+        [Description("The name of the property whose value is extracted")]
+        public string CookieName { get; set; }
+
+
+        public override void Extract(object sender, ExtractionEventArgs e)
+        {
+            if (null == e.Response) {
+                e.Success = false;
+                e.Message = String.Format("Response was null");
+                return;
+            }
+
+            if (e.Response.Cookies.Count == 0 ) {
+                e.Success = false;
+                e.Message = String.Format("No Cookies were found");
+                return;
+            }
+
+            var c = e.Response.Cookies[CookieName];
+
+            if (null == c) {
+                e.Success = false;
+                e.Message = String.Format(CultureInfo.CurrentCulture, "Cookie with name: {0} was not found", CookieName);
+                return;
+            }
+
+            e.WebTest.Context.Add(ContextParameterName, c.Value);
+        }
+    }
+}
---
+++ ExtractJsonPropertyRule.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.WebTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPTCustomExtractionRules
+{
+
+    [DisplayName("Extract JSON Property Rule")]
+    [Description("Extracts the specified JSON property from an object.")]
+    public class ExtractJsonPropertyRule : ExtractionRule
+    {
+    
+        [DisplayName("Json Property Name")]
+        [Description("The name of the property whose value is extracted")]
+        public string JsonProperty { get; set; }
+    
+        public override void Extract(object sender, ExtractionEventArgs e)
+        {
+
+            if (null == e.Response) {
+                e.Success = false;
+                e.Message = String.Format("Response was null");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(e.Response.BodyString)) {
+                e.Success = false;
+                e.Message = String.Format("Response body was empty");
+                return;
+            }
+
+            var d = JObject.Parse(e.Response.BodyString);
+
+            if (null == d) {
+                e.Success = false;
+                e.Message = String.Format("Could not parse response");
+                return;
+            }
+
+            if (null == d[JsonProperty]) {
+                e.Success = false;
+                e.Message = String.Format(CultureInfo.CurrentCulture, "Property with name: {0} not found", JsonProperty);
+                return;
+            }
+
+            e.WebTest.Context.Add(ContextParameterName, d[JsonProperty]);
+        }
+    }
+
+}
